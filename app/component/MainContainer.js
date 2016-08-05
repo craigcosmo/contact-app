c256a2a30c41185484afd88eca6ee3c7d55b05ee
Craig Nguyen <honglinh@siliconstraits.com>
2016-08-05 12:16:51 +0700

    update bootstrapcss

--- app/component/MainContainer.js
+++ app/component/MainContainer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Search from '../component/Search'
 import AddButton from '../component/AddButton'
 import '../sass/main.scss'
+import '../sass/bootstrap.scss'
 
 export default class MainContainer extends React.Component{
 	render(){ 	
@@ -11,8 +12,6 @@ export default class MainContainer extends React.Component{
 					<h2 class="text-center title-text">Contact Book</h2>
 					<AddButton />
 				</div>
-				
-
 				{this.props.children}
 			</div>
 		)
